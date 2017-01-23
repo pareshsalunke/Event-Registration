e2a8ab773b49994be0da92b39269da2a270d8b6d
psalunke <pareshsalunke@gmail.com>
2017-01-23 14:24:58 -0500

    Added form to create new event and Created the Save and Cancel dummy function

--- app/js/controllers/EditEventController.js
+++ app/js/controllers/EditEventController.js
@@ -4,8 +4,11 @@
 
 eventsApp.controller('EditEventController',
     function EditEventController($scope) {
-        $scope.saveEvent = function(event) {
-            window.alert('event ' + event.name + ' saved!');
+        $scope.saveEvent = function(event,newEventForm) {
+            console.log(newEventForm);
+            if(newEventForm.$valid) {
+                window.alert('event ' + event.name + ' saved!');
+            }
         };
 
         $scope.cancelEvent = function() {
